311a8e81329229b015d6a197aab1106d0a402136
sikali <sikali.alexander@gmail.com>
2018-07-09 16:17:46 -0700

    Update yeah.py

--- yeah.py
+++ yeah.py
@@ -1,11 +1,12 @@
-import tensor as t
+
+df = pd.read_csv('erpdata.csv', header=None)
+t = np.arange(-5, 5+0.05, 0.05)
+
+fig, ax = plt.subplots()
+ax.plot(t, df.iloc[0, :])
+ax.set_xlabel('Time (s)')
+ax.set_ylabel(r'Voltage ($\mu$V)')
+ax.set_title("Trial 1")
+plt.show()
 
 
-
-for in 
-
-
-while is in 
-
-print('hello') 
-  end
